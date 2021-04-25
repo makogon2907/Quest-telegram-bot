eeae94dbfed252f715c3e0dfe9a33394e72170c9
artem <artem.makogon.nsk@mail.ru>
2021-04-25 21:21:42 +0300

    add plan of classes

--- game.py
+++ game.py
@@ -1,19 +1,6 @@
 import config
 from spreadsheet import Spreadsheet
 
-class Problem:
-    def __init__(self, beforeproblem, text, answer, correct, incorrect, reusable):
-        self.beforeproblem = beforeproblem
-        self.text = text
-        self.answer = answer
-        self.correct = correct
-        self.incorrect = incorrect
-        self.reusable = int(reusable)
-        self.solved = 0
-        self.tried = 0
-
-
-
 labyrinth_sheet = Spreadsheet('creds.json')
 labyrinth_sheet.setSpreadsheetById(config.labirint_id)
 
@@ -38,7 +25,7 @@ teleportposition = [0, 0]
 backteleport = [0, 0]
 finishteleport = [0, 0]
 
-#TODO Осторожно, ниже много костылей
+# TODO Осторожно, ниже много костылей
 
 for i in range(len(field)):
     for j in range(len(field[i])):
@@ -54,6 +41,18 @@ for i in range(len(field)):
             backteleport = [i, j]
 
 
+class Problem:
+    def __init__(self, beforeproblem, text, answer, correct, incorrect, reusable):
+        self.beforeproblem = beforeproblem
+        self.text = text
+        self.answer = answer
+        self.correct = correct
+        self.incorrect = incorrect
+        self.reusable = int(reusable)
+        self.solved = 0
+        self.tried = 0
+
+
 for cur in arproblems:
     toadd = Problem(cur[1], cur[2], cur[3], cur[4], cur[5], cur[6])
     problems[cur[0]] = toadd
@@ -62,6 +61,54 @@ for cur in arreactions:
     if len(cur) == 2:
         reactions[cur[0]] = cur[1]
 
+
 def getCharFromField(pos):
     return field[pos[0]][pos[1]]
 
+
+class ReplyKeyboard:
+    def __init__(self):
+        pass
+
+
+class SolvingSession:
+    def __init__(self, correct_answer=''):
+        self.correct_answer = ''
+        self.is_opened = False
+
+    def make_attempt(self, answer_text):
+        pass
+
+    def open_session(self):
+        pass
+
+    def close_session(self):
+        pass
+
+    def confirm(self, message):
+        pass
+
+
+class Student:
+    def __init__(self, id: int):
+        self.id = id
+        self.position = (0, 0)
+        self.solving_session = SolvingSession()
+        self.solved_problems = set()
+
+    def move_to_new_position(self, new_position):
+        pass
+
+    def submit_answer(self, message):
+        pass
+
+
+class GameHost:
+    def __init__(self):
+        self.players = dict()  # [id, Student]
+
+    def register(self, student_id):
+        self.players[student_id] = Student(student_id)
+
+    def make_action(self, student_id: int, message: str):
+        pass
